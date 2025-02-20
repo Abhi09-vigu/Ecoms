e37088800ecd8bdac9e6d6d3da18431e0140d76c
Abhi <abhivignesh27@gmail.com>
2025-02-20 17:30:28 +0530

    Done

--- Back-end/app.js
+++ Back-end/app.js
@@ -20,16 +20,7 @@ app.use("/user",userRoute)
 
 
 
-// app.post("/create",catchAsyncErrors(async(req,res,next)=>{
-//   const{name,email,password}=req.body;
-//   if(name&&email&&password){
-//     const newUser=new UserModel({
-//       name,email,password
-//   })
-//   await newUser.save();
-//   res.status(200).send({msg:"Successful"})
-//   }
-// }))
+
 // app.use(Errorhandle)
 
 module.exports = { app };
